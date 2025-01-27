d3f7ecde677792e40c9bc1da52f1e3e90bb01d24
ArleyDev14 <arleydev14@gmail.com>
2025-01-27 17:21:54 -0500

    feat: :sparkles: CreateController

--- src/main/java/com/ejercicio3/ej_3/controller/ej_3.java
+++ src/main/java/com/ejercicio3/ej_3/controller/ej_3.java
@@ -1,5 +1,8 @@
 package com.ejercicio3.ej_3.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.ejercicio3.ej_3.Clientes;
@@ -8,8 +11,14 @@ import com.ejercicio3.ej_3.Clientes;
 public class ej_3 {
 
     @GetMapping("/api/clientes")
-    public Clientes getcliente(){
-        return new Clientes("Arley", 18);
+    public List<Clientes> getcliente(){
+
+        List<Clientes> clientes = new ArrayList<>();
+        clientes.add(new Clientes("Arley", 18));
+        clientes.add(new Clientes("Inge", 26));
+        clientes.add(new Clientes("Carlos", 30));
+
+        return clientes;
     }
 
 }
